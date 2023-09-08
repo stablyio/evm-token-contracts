d3a6b464f3d5e6b13e08d92d3eb7a1e90fed847b
David Zhang <david.d.zhang@gmail.com>
2023-09-08 12:20:43 -0700

    Add tests for seize

--- test/TRC25Stablecoin.ts
+++ test/TRC25Stablecoin.ts
@@ -66,27 +66,42 @@ describe("TRC25Stablecoin", function () {
     it("should not allow frozen addresses to transfer or burn", async function () {
       await token.mint(await random1.getAddress(), 100);
       await token.connect(compliance).freeze(await random1.getAddress());
-      await expect(
+      expect(
         token.connect(random1).transfer(await random2.getAddress(), 100)
       ).to.be.revertedWith("TRC25Compliance: account is frozen");
-      await expect(token.connect(random1).burn(100)).to.be.revertedWith(
+      expect(token.connect(random1).burn(100)).to.be.revertedWith(
         "TRC25Compliance: account is frozen"
+      );
+    });
+
+    it("should allow compliance to seize tokens", async function () {
+      await token.mint(await random1.getAddress(), 100);
+      await token.connect(compliance).seize(await random1.getAddress(), 100);
+      expect(await token.balanceOf(await random1.getAddress())).to.equal(0);
+    });
+
+    it("non-compliance role cannot seize tokens", async function () {
+      await token.mint(await random1.getAddress(), 100);
+      expect(
+        token.connect(random2).seize(await random1.getAddress(), 100)
+      ).to.be.revertedWith(
+        /AccessControl: account 0x[0-9a-fA-F]+ is missing role 0x442a94f1a1fac79af32856af2a64f63648cfa2ef3b98610a5bb7cbec4cee6985/
       );
     });
 
     it("only compliance role can freeze, unfreeze and seize", async function () {
       await token.mint(await random1.getAddress(), 100);
-      await expect(
+      expect(
         token.connect(random1).freeze(await random1.getAddress())
       ).to.be.revertedWith(
         /AccessControl: account 0x[0-9a-fA-F]+ is missing role 0x442a94f1a1fac79af32856af2a64f63648cfa2ef3b98610a5bb7cbec4cee6985/
       );
-      await expect(
+      expect(
         token.connect(random1).unfreeze(await random1.getAddress())
       ).to.be.revertedWith(
         /AccessControl: account 0x[0-9a-fA-F]+ is missing role 0x442a94f1a1fac79af32856af2a64f63648cfa2ef3b98610a5bb7cbec4cee6985/
       );
-      await expect(
+      expect(
         token.connect(random1).seize(await random1.getAddress(), 100)
       ).to.be.revertedWith(
         /AccessControl: account 0x[0-9a-fA-F]+ is missing role 0x442a94f1a1fac79af32856af2a64f63648cfa2ef3b98610a5bb7cbec4cee6985/
