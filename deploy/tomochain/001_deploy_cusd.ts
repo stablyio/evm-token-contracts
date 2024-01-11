438ae08e2af1fca35f9236b73d1c66f4a604142f
David Zhang <david.d.zhang@gmail.com>
2024-01-10 21:00:42 -0800

    Redeploy CUSD on Viction testnet

--- deploy/tomochain/001_deploy_cusd.ts
+++ deploy/tomochain/001_deploy_cusd.ts
@@ -14,17 +14,17 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
   });
   // Set the minter role
-  const usdv = await hre.ethers.getContractAt(
+  const cusd = await hre.ethers.getContractAt(
     "TRC25Stablecoin",
     deployed.address
   );
-  await usdv.grantRole(await usdv.MINTER_ROLE(), minter, {
+  await cusd.grantRole(await cusd.MINTER_ROLE(), minter, {
     // Need to hard-code a gasLimit due to not supporting modern `eth_estimateGas`
     // See: https://github.com/NomicFoundation/hardhat/issues/4010
     gasLimit: "0x10000",
   });
   // Set the fee role
-  await usdv.grantRole(await usdv.FEE_ROLE(), feeRole, {
+  await cusd.grantRole(await cusd.FEE_ROLE(), feeRole, {
     gasLimit: "0x10000",
   });
 };
